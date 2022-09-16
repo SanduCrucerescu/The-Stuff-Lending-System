a015e48397b1f69c6eca5f1abd6bfba5ea511ccf
Sandu Crucerescu <alexcrucerescu82@gmail.com>
2022-09-16 15:45:32 +0200

    Updating the pipeline

---
+++ Tests/Assigment 2Tests/Assigment_2Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Assigment_2
+
+final class Assigment_1Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        //XCTAssertEqual(Assigment_1().text, "Hello, World!")
+    }
+}
